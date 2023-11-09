d9792a8563473b25635867b1fbfa513e818e281c
laureanray <laureanraybahala@gmail.com>
2023-11-09 12:58:41 +0800

    progress: event handling

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ var app = new Vue({
     el: '#app',
     data: {
         image: "./assets.png",
-        altText: "test",
         product: "Socks",
-        link: "/some-random-url",
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Genter-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+            }
+        ],
+        cart: 0
     }
 });
