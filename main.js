ae9b08b71c1d9ae9250c9569b6a68ad82f636e20
laureanray <laureanraybahala@gmail.com>
2023-11-11 19:39:44 +0800

    convert to component

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand: "Vue Mastery",
-        selectedVariant: 0,
-        details: ["80% cotton", "20% polyester", "Genter-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: "./variant-green.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: "./variant-blue.jpg",
-                variantQuantity: 0
-            }
-        ],
-        cart: 0
+Vue.component('product', {
+    template: `
+        <div class="product">
+            <div class="product-image">
+                <img :src="image">
+            </div>
+
+            <div class="product-info">
+                <h1> {{ product }}</h1>
+                <p v-if="inStock"> In Stock </p>
+                <p v-else>Out of Stock</p>
+
+                <ul>
+                    <li v-for="detail in details"> {{ detail }}</li>
+                </ul>
+
+                <div @mouseover="updateProduct(index)" class="color-box" v-for="(variant, index) in variants"
+                    :key="variant.variantId" :style="{ backgroundColor: variant.variantColor}">
+                </div>
+
+                <div class="cart">
+                    <p> Cart ({{cart}})</p>
+                </div>
+
+                <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock}"> Add to
+                    cart</button>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            product: "Socks",
+            brand: "Vue Mastery",
+            selectedVariant: 0,
+            details: ["80% cotton", "20% polyester", "Genter-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: "./variant-green.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: "./variant-blue.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0
+        }
     },
     methods: {
         addToCart() {
@@ -40,5 +69,9 @@ var app = new Vue({
             return this.variants[this.selectedVariant].variantQuantity
         }
     }
+});
+
+var app = new Vue({
+    el: '#app',
 });
 
